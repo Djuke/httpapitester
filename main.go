c25ade37e6f17df68f43d38c3721e21a8ae32fcb
Djuke van Hoof <djuke.vanhoof@icasting.com>
2015-08-25 17:05:04 +0200

    v2.1.0

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	fmt.Println("v2.0.0")
+	fmt.Println("v2.1.0")
 	if len(os.Args) != 2 || os.Args[1] == "" {
 		fmt.Println("HTTP API tester is a tool to test HTTP APIs\n\nusage: httpapitester [test suite file]\n")
 		os.Exit(0)
